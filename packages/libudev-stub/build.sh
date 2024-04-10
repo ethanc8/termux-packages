c8dc94a5b13072e239fc095601a817d81e678724
xMeM <haooy@outlook.com>
2024-04-10 13:08:11 +0800

    new package: libudev-stub

---
+++ packages/libudev-stub/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE="https://github.com/therealkenc/libudev-stub"
+TERMUX_PKG_DESCRIPTION="A libudev stub and proxy library for WSL"
+TERMUX_PKG_LICENSE="LGPL-2.1"
+TERMUX_PKG_MAINTAINER="xMem <haooy@outlook.com>"
+TERMUX_PKG_VERSION="1.0.4"
+TERMUX_PKG_SRCURL="git+https://github.com/therealkenc/libudev-stub.git"
+TERMUX_PKG_CONFLICTS="libudev"
